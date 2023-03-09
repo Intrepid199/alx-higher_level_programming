c6ee929ef37497f55195395bf384b9f83644afea
Intrepid199 <hassantaofeek0123@gmail.com>
2023-03-09 06:09:30 -0800

    calculating py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,12 +4,11 @@ if __name__ == "__main__":
     args = len(sys.argv)
     print("{} ".format(args - 1), end="")
     if args == 1:
-         print("arguments.")
+        print("arguments.")
     elif args > 1:
         if args == 2:
             print("argument:")
         elif args > 2:
             print("arguments:")
-        for i in range(args - 1):
-            print("{}: {}".format((i + 1), sys.argv[i + 1]))
-
+            for i in range(args - 1):
+                print("{}: {}".format((i + 1), sys.argv[i + 1]))
