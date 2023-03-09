063de988ab0df44daaa3f7616214871bfb178a95
Intrepid199 <hassantaofeek0123@gmail.com>
2023-03-09 07:52:36 -0800

    new calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     args = sys.argv
     if len(args) != 4:
-         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-         sys.exit(1)
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
     if args[2] != '+' and args[2] != '-' and args[2] != '*' and args[2] != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
@@ -19,3 +19,4 @@ if __name__ == "__main__":
         print("{} {} {} = {}".format(a, args[2], b, mul(a, b)))
     elif args[2] == '/':
         print("{} {} {} = {}".format(a, args[2], b, div(a, b)))
+
